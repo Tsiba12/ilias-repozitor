2e990cb9a2db75e06f804426760a062d99e7aa2f
Tsiba12 <112581006+Tsiba12@users.noreply.github.com>
2022-09-27 23:49:02 +0400

    Add files via upload

---
+++ lessen4.py
@@ -0,0 +1,14 @@
+name = "ilia"
+
+print(len(name))
+
+fulname = "ilia tsibadze"
+
+print(len(fulname))
+
+my_sentence = "გამარჯობა მე ილია ვარ მე მიყვარს ახალი ჰობიების შეძენა და ერთერთი ჩემი საყვარელი არის პროგრამირება"
+
+print(my_sentence)
+
+if 20<13:
+    print("my cat is cute")
